dcad4d0461167f3db4652f085e325a867506ee8a
Wintan <wintan1418@gmail.com>
2026-05-04 23:30:27 +0100

    Make home page a public feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,19 @@ class HomeController < ApplicationController
     if user_signed_in?
       redirect_to feed_path
     else
-      @rooms = Room.public_rooms.ordered.limit(6)
-      @featured_posts = Post.published.featured.includes(:user, :room).limit(3)
+      @rooms = Room.public_rooms.ordered.limit(8)
+      @featured_rooms = @rooms.first(4)
+      @posts = Post.published.includes(:user, :room, :tags).recent.limit(8)
+      @prayer_posts = Post.published
+                          .where(room_id: Room.prayers.public_rooms.select(:id))
+                          .includes(:user, :room)
+                          .recent
+                          .limit(3)
+      @trending_posts = Post.published
+                            .includes(:user, :room)
+                            .order(Arel.sql("(likes_count * 2 + comments_count * 3 + views_count * 0.1) DESC"))
+                            .limit(4)
+      @resources = Resource.approved.featured.includes(:resource_category).recent.limit(4)
     end
   end
 end
