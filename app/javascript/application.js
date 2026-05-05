cc9bdd205beb75068ee4d453a337ec6202c1b5f7
Wintan <wintan1418@gmail.com>
2026-05-05 14:51:11 +0100

    Use server-side Cloudinary uploads

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as ActiveStorage from "@rails/activestorage"
 
 import "trix"
 import "@rails/actiontext"
 import "emoji-picker-element"
-
-ActiveStorage.start()
