12d4e2d4e090e85eba617ac99b8ddce48ff1038f
Wintan <wintan1418@gmail.com>
2026-05-05 18:00:41 +0100

    Fix admin room icon rendering

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,9 +1,9 @@
 module IconHelper
   def render_icon(name, classes: "w-6 h-6")
-    icon_name = name.to_s.downcase
+    icon_name = name.to_s.downcase.gsub(/[^a-z0-9]/, "")
 
     case icon_name
-    when "heart"
+    when "heart", "hearthandshake"
       icon_svg("M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z", classes)
     when "sprout"
       icon_svg("M2 22s5-3 9-11 5-11 5-11M16 11s0 5 6 11", classes)
@@ -13,11 +13,11 @@ module IconHelper
       icon_svg("M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2 M16 3.13a4 4 0 0 1 0 7.75 M23 21v-2a4 4 0 0 0-3-3.87", classes)
     when "leaf"
       icon_svg("M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12", classes)
-    when "messagecircle", "message_circle"
+    when "messagecircle", "message_circle", "messagecirclequestion"
       icon_svg("M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z", classes)
     when "lightbulb"
       icon_svg("M15 14c.2-1 .7-1.7 1.5-2.5 1-1 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5", classes) + icon_path("M9 18h6 M10 22h4", classes)
-    when "handheart", "hand_heart"
+    when "handheart", "hand_heart", "pray", "prayer"
       icon_svg("M11 14h2a2 2 0 1 0 0-4h-2V7h4 M13 14v7 M7 7h10", classes) # Simplified
     when "shield"
       icon_svg("M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z", classes)
@@ -43,6 +43,8 @@ module IconHelper
       icon_svg("M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16 M2 10h20 M2 21h20", classes)
     when "goal"
       icon_svg("M12 13V2l8 4-8 4 M20.55 10.23A9 9 0 1 1 8 4.94", classes)
+    when "route"
+      icon_svg("M6 19a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z M18 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z M8.6 14.6 15.4 9.4 M18 11v2a4 4 0 0 1-4 4H9", classes)
     when "globe"
       icon_svg("M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20z M2 12h20 M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z", classes)
     when "send"
